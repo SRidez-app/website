09637529ab9801dc844de6c52b7bcb41447e568a
abdullahabbasi143 <129058605+abdullahabbasi143@users.noreply.github.com>
2025-05-28 23:47:27 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/logoFooter.png';
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
