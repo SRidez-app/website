97a2fbc29655a0db154c8628bd28e95684362cac
SRidez_app <adibayubzai1@gmail.com>
2025-07-21 15:48:53 -0500

    removed footer link

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Footer from "../components/Newfooter";
+
 
 
 
@@ -311,7 +311,7 @@ const Login = () => {
         }
       `}</style>
     
-       <Footer/>
+
       </div>
   );
 };
