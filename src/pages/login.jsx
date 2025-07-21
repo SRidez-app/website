cbbe0ef3304236a70c94a5a82839188544b8c8a4
SRidez_app <adibayubzai1@gmail.com>
2025-07-21 15:39:28 -0500

    type in footer

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Footer from "../../components/Newfooter";
+import Footer from "../components/Newfooter";
 
 
 
